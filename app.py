4161b2223f1d17dafbb47bcb958f21b271f84eaa
erio_y <erioyoshino99@gmail.com>
2025-03-17 00:22:57 +0700

    created generate image, and cleaned up stuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import flash, Flask, render_template, request
 from preprocess import data_prep
+from model import model_loader
 import time
 
 
@@ -11,14 +12,37 @@ app = Flask(__name__)
 def homepage():
     return render_template('content.html', data = data_prep.get_all_path())
 
+
+def generate(prompt):
+    data_prep.preprocess()
+    output_path = model_loader.generate_image(
+        canny_image_path=data_prep.canny_image_path,
+        prompt=prompt,
+        upload_dir=data_prep.get_upload_folder(),
+        image_name=data_prep.get_curr_image_name(),
+        extension=data_prep.get_curr_image_extension()
+    )
+    data_prep.result_image_path = output_path
+
+
 @app.route('/', methods=['POST'])
 def image_upload():
-    status, message = data_prep.verify_image_upload()
+    image_status = data_prep.verify_image_upload(request)
+
     # file uploaded doesn't met the requirement
-    if not status:
+    if not image_status["status"]:
         return render_template("content.html", data=data_prep.get_all_path())
+    
     # save
-    data_prep.save_image_to_upload(image_to_save=request.files['uploadFile'], extension=message)
+    data_prep.save_image_to_upload(image_to_save=request.files['uploadFile'], extension=image_status["message"])
+    
+    text_prompt = request.form['textPrompt']
+    # in case  of something
+    if text_prompt is None:
+        text_prompt = ""
+    
+    generate(text_prompt)
+
     return render_template("content.html", data=data_prep.get_all_path())
 
 
@@ -34,25 +58,24 @@ def flash_test():
         print("Hellow")
     uploaded_file = request.files['uploadFile']
     if uploaded_file.filename == '':
-        flash("nibba")
+        flash("nofile")
 
     return render_template('test.html')
 
 
 if __name__ == "__main__":
     # create folder check (if none, create)
-    status = False
-    if not status:
-        print("HELLO")
-    # app.config['UPLOAD_FOLDER'] = image_editor.get_upload_path()
-    # app.secret_key = "testdoang"
-    # app.run(debug=True)
+    app.config['UPLOAD_FOLDER'] = data_prep.get_upload_path()
+    app.secret_key = "testdoang"
+    app.run(debug=True)
 
 """
 TO-DO:
 
 [ ] implement load model
 [ ] change model
+[ ] implement logger for easy read
+[ ] make load model a button (easier debug purpose :)
 
 Extra stuff.
 [ ] make 
